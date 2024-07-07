2787f8e30cd24ec8c504789a4ca42a2df2640396
Daniel <danielcoblentz916@gmail.com>
2024-07-07 19:01:28 -0400

    struggle

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const dropdownFooter = document.querySelector(".dropdown-footer");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+    dropdownFooter.classList.toggle("active");
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    dropdownFooter.classList.remove("active");
+}));
